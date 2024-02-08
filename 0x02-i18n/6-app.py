ca7a90508f4f37de92ecbe2e477580f89fde9de6
hillary-254 <hillarykim001@gmail.com>
2024-02-08 16:33:11 +0300

    minor change

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -27,7 +27,7 @@ users = {
 
 
 def get_user() -> Union[Dict, None]:
-    """Retrieves a user based on a user id
+    """Retrieves user based on a user id
     """
     login_id = request.args.get('login_as', '')
     if login_id:
