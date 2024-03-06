d4d05981f39a6f500d82bb0f1df0fce89abfbe78
hillary-254 <hillarykim001@gmail.com>
2024-03-06 15:21:37 +0300

    1. Node Redis Client

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,15 @@
+#!/usr/bin/yarn dev
+import redis from 'redis';
+
+// Create a Redis client
+const client = redis.createClient();
+
+// Listen for the 'connect' event
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+// Listen for the 'error' event
+client.on('error', (error) => {
+    console.error('Redis client not connected to the server:', error);
+});
